d99a861c76737e4f02191d02401f03be86ea9d11
Saulo <saulo.santos@sga.pucminas.br>
2018-10-15 20:46:53 -0300

    Conclusão da Tela Principal

--- Catalogo/Catalogo/Principal.Designer.cs
+++ Catalogo/Catalogo/Principal.Designer.cs
@@ -28,20 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblArtigo = new System.Windows.Forms.Label();
+            this.txtArtigo = new System.Windows.Forms.TextBox();
+            this.sptPrincipal = new System.Windows.Forms.SplitContainer();
+            this.lstCategorias = new System.Windows.Forms.ListBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.pnlInfo = new System.Windows.Forms.Panel();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lklDesfazerExclusao = new System.Windows.Forms.LinkLabel();
             this.grdArtigos = new System.Windows.Forms.DataGridView();
             this.lblTitulo = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblArtigo = new System.Windows.Forms.Label();
-            this.txtCategoria = new System.Windows.Forms.TextBox();
-            this.txtArtigo = new System.Windows.Forms.TextBox();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.lstCategorias = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sptPrincipal)).BeginInit();
+            this.sptPrincipal.Panel1.SuspendLayout();
+            this.sptPrincipal.Panel2.SuspendLayout();
+            this.sptPrincipal.SuspendLayout();
+            this.pnlInfo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grdArtigos)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(3, 3);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblCategoria.TabIndex = 2;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // lblArtigo
+            // 
+            this.lblArtigo.AutoSize = true;
+            this.lblArtigo.Location = new System.Drawing.Point(3, 3);
+            this.lblArtigo.Name = "lblArtigo";
+            this.lblArtigo.Size = new System.Drawing.Size(34, 13);
+            this.lblArtigo.TabIndex = 2;
+            this.lblArtigo.Text = "Artigo";
+            // 
+            // txtArtigo
+            // 
+            this.txtArtigo.Location = new System.Drawing.Point(40, 0);
+            this.txtArtigo.Name = "txtArtigo";
+            this.txtArtigo.Size = new System.Drawing.Size(527, 20);
+            this.txtArtigo.TabIndex = 4;
+            this.txtArtigo.TextChanged += new System.EventHandler(this.txtArtigo_TextChanged);
+            // 
+            // sptPrincipal
+            // 
+            this.sptPrincipal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sptPrincipal.Location = new System.Drawing.Point(12, 47);
+            this.sptPrincipal.Name = "sptPrincipal";
+            // 
+            // sptPrincipal.Panel1
+            // 
+            this.sptPrincipal.Panel1.Controls.Add(this.lstCategorias);
+            this.sptPrincipal.Panel1.Controls.Add(this.txtCategoria);
+            this.sptPrincipal.Panel1.Controls.Add(this.lblCategoria);
+            // 
+            // sptPrincipal.Panel2
+            // 
+            this.sptPrincipal.Panel2.Controls.Add(this.pnlInfo);
+            this.sptPrincipal.Panel2.Controls.Add(this.grdArtigos);
+            this.sptPrincipal.Panel2.Controls.Add(this.txtArtigo);
+            this.sptPrincipal.Panel2.Controls.Add(this.lblArtigo);
+            this.sptPrincipal.Size = new System.Drawing.Size(803, 485);
+            this.sptPrincipal.SplitterDistance = 215;
+            this.sptPrincipal.TabIndex = 5;
+            // 
+            // lstCategorias
+            // 
+            this.lstCategorias.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstCategorias.FormattingEnabled = true;
+            this.lstCategorias.Location = new System.Drawing.Point(3, 26);
+            this.lstCategorias.Name = "lstCategorias";
+            this.lstCategorias.Size = new System.Drawing.Size(209, 446);
+            this.lstCategorias.TabIndex = 0;
+            this.lstCategorias.SelectedIndexChanged += new System.EventHandler(this.lstCategorias_SelectedIndexChanged);
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(58, 0);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(150, 20);
+            this.txtCategoria.TabIndex = 3;
+            this.txtCategoria.TextChanged += new System.EventHandler(this.txtCategoria_TextChanged);
+            // 
+            // pnlInfo
+            // 
+            this.pnlInfo.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pnlInfo.Controls.Add(this.lblInfo);
+            this.pnlInfo.Controls.Add(this.lklDesfazerExclusao);
+            this.pnlInfo.Location = new System.Drawing.Point(177, 449);
+            this.pnlInfo.Name = "pnlInfo";
+            this.pnlInfo.Size = new System.Drawing.Size(400, 27);
+            this.pnlInfo.TabIndex = 6;
+            this.pnlInfo.Visible = false;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.BackColor = System.Drawing.Color.Transparent;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.Black;
+            this.lblInfo.Location = new System.Drawing.Point(15, 5);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(218, 17);
+            this.lblInfo.TabIndex = 8;
+            this.lblInfo.Text = "Artigo excluído com sucesso.";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lklDesfazerExclusao
+            // 
+            this.lklDesfazerExclusao.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lklDesfazerExclusao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lklDesfazerExclusao.LinkColor = System.Drawing.Color.Red;
+            this.lklDesfazerExclusao.Location = new System.Drawing.Point(258, 0);
+            this.lklDesfazerExclusao.Name = "lklDesfazerExclusao";
+            this.lklDesfazerExclusao.Size = new System.Drawing.Size(142, 27);
+            this.lklDesfazerExclusao.TabIndex = 7;
+            this.lklDesfazerExclusao.TabStop = true;
+            this.lklDesfazerExclusao.Text = "Desfazer Exclusão";
+            this.lklDesfazerExclusao.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lklDesfazerExclusao.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lklDesfazerExclusao_LinkClicked);
             // 
             // grdArtigos
             // 
@@ -53,121 +165,65 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.grdArtigos.BackgroundColor = System.Drawing.SystemColors.Window;
             this.grdArtigos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grdArtigos.Location = new System.Drawing.Point(3, 3);
+            this.grdArtigos.Location = new System.Drawing.Point(3, 26);
             this.grdArtigos.Name = "grdArtigos";
             this.grdArtigos.ReadOnly = true;
             this.grdArtigos.RowHeadersVisible = false;
-            this.grdArtigos.Size = new System.Drawing.Size(564, 497);
+            this.grdArtigos.Size = new System.Drawing.Size(578, 455);
             this.grdArtigos.TabIndex = 0;
+            this.grdArtigos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdArtigos_CellContentClickAsync);
             this.grdArtigos.Resize += new System.EventHandler(this.grdArtigos_Resize);
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Location = new System.Drawing.Point(364, 9);
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(329, 9);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(79, 13);
+            this.lblTitulo.Size = new System.Drawing.Size(146, 25);
             this.lblTitulo.TabIndex = 1;
             this.lblTitulo.Text = "Lista de Artigos";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(201, 57);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Categoria";
-            // 
-            // lblArtigo
-            // 
-            this.lblArtigo.AutoSize = true;
-            this.lblArtigo.Location = new System.Drawing.Point(409, 57);
-            this.lblArtigo.Name = "lblArtigo";
-            this.lblArtigo.Size = new System.Drawing.Size(34, 13);
-            this.lblArtigo.TabIndex = 2;
-            this.lblArtigo.Text = "Artigo";
-            // 
-            // txtCategoria
-            // 
-            this.txtCategoria.Location = new System.Drawing.Point(259, 54);
-            this.txtCategoria.Name = "txtCategoria";
-            this.txtCategoria.Size = new System.Drawing.Size(100, 20);
-            this.txtCategoria.TabIndex = 3;
-            // 
-            // txtArtigo
-            // 
-            this.txtArtigo.Location = new System.Drawing.Point(451, 54);
-            this.txtArtigo.Name = "txtArtigo";
-            this.txtArtigo.Size = new System.Drawing.Size(100, 20);
-            this.txtArtigo.TabIndex = 4;
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.splitContainer1.Location = new System.Drawing.Point(12, 95);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.lstCategorias);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.grdArtigos);
-            this.splitContainer1.Size = new System.Drawing.Size(785, 503);
-            this.splitContainer1.SplitterDistance = 211;
-            this.splitContainer1.TabIndex = 5;
-            // 
-            // lstCategorias
-            // 
-            this.lstCategorias.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lstCategorias.FormattingEnabled = true;
-            this.lstCategorias.Location = new System.Drawing.Point(3, 3);
-            this.lstCategorias.Name = "lstCategorias";
-            this.lstCategorias.Size = new System.Drawing.Size(205, 485);
-            this.lstCategorias.TabIndex = 0;
-            this.lstCategorias.SelectedIndexChanged += new System.EventHandler(this.lstCategorias_SelectedIndexChanged);
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(809, 610);
-            this.Controls.Add(this.splitContainer1);
-            this.Controls.Add(this.txtArtigo);
-            this.Controls.Add(this.txtCategoria);
-            this.Controls.Add(this.lblArtigo);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(827, 556);
+            this.Controls.Add(this.sptPrincipal);
             this.Controls.Add(this.lblTitulo);
             this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Principal";
             this.Load += new System.EventHandler(this.Principal_Load);
+            this.Resize += new System.EventHandler(this.Principal_Resize);
+            this.sptPrincipal.Panel1.ResumeLayout(false);
+            this.sptPrincipal.Panel1.PerformLayout();
+            this.sptPrincipal.Panel2.ResumeLayout(false);
+            this.sptPrincipal.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sptPrincipal)).EndInit();
+            this.sptPrincipal.ResumeLayout(false);
+            this.pnlInfo.ResumeLayout(false);
+            this.pnlInfo.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grdArtigos)).EndInit();
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView grdArtigos;
-        private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCategoria;
         private System.Windows.Forms.Label lblArtigo;
-        private System.Windows.Forms.TextBox txtCategoria;
         private System.Windows.Forms.TextBox txtArtigo;
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer sptPrincipal;
         private System.Windows.Forms.ListBox lstCategorias;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Panel pnlInfo;
+        private System.Windows.Forms.LinkLabel lklDesfazerExclusao;
+        private System.Windows.Forms.DataGridView grdArtigos;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
 
--- Catalogo/Catalogo/Principal.cs
+++ Catalogo/Catalogo/Principal.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using DataGridViewButton;
+using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Catalogo
 {
     public partial class Principal : Form
     {
+        int msDelay = 3000;
+        CancellationTokenSource tokenSource = new CancellationTokenSource();
+
         public Principal()
         {
             InitializeComponent();
@@ -16,8 +22,59 @@ namespace Catalogo
         {
             LstCategoriaPreencher();
             GridArtigosPreencher();
-            GridArtigosFormatar();
+        }
+
+        private void Principal_Resize(object sender, EventArgs e)
+        {
+            lblTitulo.Left = (this.Width - lblTitulo.Width) / 2;
+
+            txtCategoria.Width = lstCategorias.Width - lblCategoria.Width - 2;
+            txtArtigo.Width = grdArtigos.Width - lblArtigo.Width - 2;
+
             GridArtigosRedimensionar();
+        }
+
+        private void txtCategoria_TextChanged(object sender, EventArgs e)
+        {
+            LstCategoriaPreencher();
+        }
+
+        private void txtArtigo_TextChanged(object sender, EventArgs e)
+        {
+            GridArtigosPreencher();
+        }
+
+        private void lstCategorias_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridArtigosPreencher();
+        }
+
+        private async void grdArtigos_CellContentClickAsync(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex < 0 || e.RowIndex < 0) return;
+
+            int colSel = e.ColumnIndex;
+            int rowSel = e.RowIndex;
+
+            int artigoCodigo = int.Parse(grdArtigos.Rows[rowSel].Cells[0].Value.ToString());
+
+            var cellEnabled = ((DataGridViewDisableButtonCell)grdArtigos.Rows[rowSel].Cells[5]).Enabled;
+
+            if (e.ColumnIndex == 5 && cellEnabled)
+            {
+                grdArtigos.Rows.RemoveAt(rowSel);
+                await ArtigoExcluirAsync(artigoCodigo);
+            }
+        }
+
+        private void grdArtigos_Resize(object sender, EventArgs e)
+        {
+            //GridArtigosRedimensionar();
+        }
+
+        private void lklDesfazerExclusao_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            tokenSource.Cancel();
         }
 
         private void LstCategoriaPreencher()
@@ -25,21 +82,30 @@ namespace Catalogo
             SqlConnection cnn = new SqlConnection();
             string pstrMsg = "";
             bool pbooRetorno = false;
+            string titCategoria = txtCategoria.Text;
 
             cnn = ConexaoBD.CriarConexao(out pstrMsg, out pbooRetorno);
 
             if (pbooRetorno)
             {
                 string strSQL = @"
-                    SELECT * FROM 
-                    (
-                    SELECT 0 CATE_CODIGO, '(TODOS)' CATE_TITULO
-                    UNION ALL
-                    SELECT 
-	                    C.CATE_CODIGO, C.CATE_TITULO
-                    FROM TB_CATEGORIA C
-                    ) CatOrdenado
-                    ORDER BY CatOrdenado.CATE_TITULO";
+                SELECT * FROM 
+                (
+                SELECT 0 CATE_CODIGO, '(TODOS)' CATE_TITULO
+                UNION ALL
+                SELECT 
+	                C.CATE_CODIGO, C.CATE_TITULO
+                FROM TB_CATEGORIA C
+                ) CatOrdenado 
+                WHERE 1 = 1 ";
+
+
+                if (!string.IsNullOrEmpty(titCategoria))
+                {
+                    strSQL += "AND CatOrdenado.CATE_TITULO LIKE '%" + titCategoria + "%' ";
+                }
+
+                strSQL += "ORDER BY CatOrdenado.CATE_TITULO ";
 
                 SqlCommand sqlCmd = new SqlCommand(strSQL, cnn);
 
@@ -60,19 +126,22 @@ namespace Catalogo
                     Close();
             }
 
+            //GridArtigosPreencher();
+
             cnn.Close();
         }
 
-        private void grdArtigos_Resize(object sender, EventArgs e)
+        private void GridArtigosPreencher()
         {
-            GridArtigosRedimensionar();
-        }
+            grdArtigos.Columns.Clear();
+            grdArtigos.DataSource = null;
 
-        private void GridArtigosPreencher(int idCategoria = 0)
-        {
             SqlConnection cnn = new SqlConnection();
             string pstrMsg = "";
             bool pbooRetorno = false;
+
+            int idCategoria = lstCategorias.SelectedIndex;
+            string titArtigo = txtArtigo.Text;
 
             cnn = ConexaoBD.CriarConexao(out pstrMsg, out pbooRetorno);
 
@@ -82,11 +151,17 @@ namespace Catalogo
                     SELECT 
                     A.ARTI_CODIGO, A.ARTI_NOME, A.ARTI_CATE_CODIGO, C.CATE_TITULO
                     FROM TB_ARTIGO A 
-                    INNER JOIN TB_CATEGORIA C ON C.CATE_CODIGO = A.ARTI_CATE_CODIGO ";
+                    INNER JOIN TB_CATEGORIA C ON C.CATE_CODIGO = A.ARTI_CATE_CODIGO 
+                    WHERE 1 = 1 ";
 
                 if (idCategoria != 0)
                 {
-                    strSQL += "WHERE C.CATE_CODIGO = " + idCategoria + " ";
+                    strSQL += "AND C.CATE_CODIGO = " + idCategoria + " ";
+                }
+
+                if (!string.IsNullOrEmpty(titArtigo))
+                {
+                    strSQL += "AND A.ARTI_NOME LIKE '%" + titArtigo + "%' ";
                 }
 
                 strSQL += "ORDER BY A.ARTI_NOME ";
@@ -96,9 +171,8 @@ namespace Catalogo
 
                 DataTable dtRecord = new DataTable();
                 sqlDataAdap.Fill(dtRecord);
-                grdArtigos.DataSource = dtRecord;
 
-                
+                grdArtigos.DataSource = dtRecord;
             }
             else
             {
@@ -107,6 +181,11 @@ namespace Catalogo
                 if (!pbooRetorno)
                     Close();
             }
+
+            cnn.Close();
+
+            GridArtigosFormatar();
+            
         }
 
         private void GridArtigosFormatar()
@@ -121,20 +200,22 @@ namespace Catalogo
 
             grdArtigos.Columns[3].HeaderText = "Categoria";
 
-            DataGridViewButtonColumn btcEditar = new DataGridViewButtonColumn();
+            DataGridViewDisableButtonColumn btcEditar = new DataGridViewDisableButtonColumn();
             btcEditar.Name = "btnEditar";
             btcEditar.HeaderText = "Editar";
             btcEditar.UseColumnTextForButtonValue = true;
             btcEditar.Text = "Editar";
 
-            DataGridViewButtonColumn btcExcluir = new DataGridViewButtonColumn();
-            btcExcluir.Name = "btcExcluir";
+            DataGridViewDisableButtonColumn btcExcluir = new DataGridViewDisableButtonColumn();
+            btcExcluir.Name = "btnExcluir";
             btcExcluir.HeaderText = "Excluir";
             btcExcluir.UseColumnTextForButtonValue = true;
             btcExcluir.Text = "Excluir";
 
             grdArtigos.Columns.Add(btcEditar);
             grdArtigos.Columns.Add(btcExcluir);
+
+            GridArtigosRedimensionar();
         }
 
         private void GridArtigosRedimensionar()
@@ -146,11 +227,80 @@ namespace Catalogo
             grdArtigos.Columns[3].Width = (int)(intGridViewWidthWoutButton * 0.25);
             grdArtigos.Columns[4].Width = intButtonWidth;
             grdArtigos.Columns[5].Width = intButtonWidth;
+
+            pnlInfo.Left = grdArtigos.Width - pnlInfo.Width;
+            pnlInfo.Top = grdArtigos.Height - 5;// - pnlInfo.Height;
         }
 
-        private void lstCategorias_SelectedIndexChanged(object sender, EventArgs e)
+        async Task AcaoDelay()
         {
-            GridArtigosPreencher(lstCategorias.SelectedIndex);
+            try
+            {
+                await Task.Delay(msDelay, tokenSource.Token);
+            }
+            catch (TaskCanceledException tcex)
+            {
+                throw tcex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                tokenSource.Dispose();
+                tokenSource = new CancellationTokenSource();
+            }
+        }
+
+        private async Task ArtigoExcluirAsync(int codArtigo)
+        {
+            pnlInfo.Visible = true;
+            ExcluirButtonEnable();
+
+            try
+            {
+                await AcaoDelay();
+
+                SqlConnection cnn = new SqlConnection();
+                string pstrMsg = "";
+                bool pbooRetorno = false;
+
+                cnn = ConexaoBD.CriarConexao(out pstrMsg, out pbooRetorno);
+
+                if (pbooRetorno)
+                {
+                    string strSQL = @"
+                    DELETE FROM TB_ARTIGO
+                    WHERE ARTI_CODIGO = " + codArtigo;
+
+                    SqlCommand sqlCmd = new SqlCommand(strSQL, cnn);
+                    sqlCmd.ExecuteNonQuery();
+
+                    GridArtigosPreencher();
+                }
+            }
+            catch (TaskCanceledException ex)
+            {
+                if (ex.CancellationToken.IsCancellationRequested)
+                {
+                    GridArtigosPreencher();
+                }                
+            } 
+            finally
+            {
+                pnlInfo.Visible = false;
+                ExcluirButtonEnable(true);
+            }
+        }
+
+        private void ExcluirButtonEnable(bool blnEnable = false)
+        {
+            foreach (DataGridViewRow row in grdArtigos.Rows)
+            {
+                DataGridViewDisableButtonCell btn = (DataGridViewDisableButtonCell)row.Cells[5];
+                btn.Enabled = blnEnable;
+            }
         }
     }
 }
